df862c9528a8c47a810fbf477a04d09b8ea655b0
joe2Dev <josephonwe7@gmail.com>
2023-09-10 12:29:16 +0000

    created category functionalities(controllers, model, routes)

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,8 +1,8 @@
-import Category from "../model/category.js";
+import categoryModel from "../model/category.js";
 
 export const getCategories = async (req, res) => {
  try{
-  const category = await Category.find();
+  const category = await categoryModel.find();
   res.status(201).json(category)
  }catch(error) {
   res.status(500).json({
@@ -14,22 +14,23 @@ export const getCategories = async (req, res) => {
 export const getCategory = async(req, res) => {
  const categoryId = req.params.categoryId
  try{
-  const category = await Category.findById(categoryId);
+  const category = await categoryModel.findById(categoryId);
   if(!category) {
     res.status(404).json({msg: "The category with the given ID was not found"}) 
   }
   res.status(200).json(category);
  }catch(error) {
   res.status(500).json({
+   msg: "Invalid Id parameter",
    success: false,
-   msg: error
+   value: error,
   })
  }
 }
 
 export const addCategory = async (req, res) => {
  try{
-  const newCategory = Category.create({
+  const newCategory = categoryModel.create({
    name: req.body.name,
    icon: req.body.icon,
    color: req.body.color,
@@ -44,11 +45,15 @@ export const addCategory = async (req, res) => {
 }
 
 export const updateCategories = async (req, res) => {
+  const { name, color, image} = req.body
  try{
-  await Category.findByIdAndUpdate(
+  await categoryModel.findByIdAndUpdate(
    req.params.categoryId,
    { 
-    $set: req.body 
+    name,
+    icon: req.body.icon || category.icon,
+    color,
+    image
    },
    {
     new: true
@@ -65,10 +70,10 @@ export const updateCategories = async (req, res) => {
 
 export const deleteCategory = async (req, res) => {
  try {
-  let category = await Category.findById(req.params.categoryId)
+  let category = await categoryModel.findById(req.params.categoryId)
   (!category) ? res.status(404).json({msg: "category not found"}) :
   await category.remove();
-  res.status(200).json("Category has been deleted successfully")
+  res.status(201).json("Category has been deleted successfully")
  }catch(error) {
   res.status(500).json({
    success: false,
