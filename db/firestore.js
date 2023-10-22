ed51709bfbae3c7fae85ceb496ef4cb953ef78e9
joe2Dev <josephonwe7@gmail.com>
2023-10-22 15:18:40 +0000

    completed the user firebase persistence functionality into the app with contextAPI and Reducers in SignInWelcomeScreen.js

--- db/firestore.js
+++ db/firestore.js
@@ -38,6 +38,12 @@ export const FIREBASE_DB = getFirestore(app);
 
 
 
+
+
+
+
+
+
 // export const FIREBASE_APP = initializeApp(firebaseConfig);
 // export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
 // export const FIREBASE_DB = getFirestore(FIREBASE_APP)
