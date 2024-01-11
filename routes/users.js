0e83a081c550deb7ff7541062c230a1b6a5516cd
joe2Dev <josephonwe7@gmail.com>
2024-01-11 17:33:45 +0000

    modified addUser routes to users/register

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,22 @@
 import express from "express";
-import { getUsers, addUser, updateUser, getUser, deleteUser, loginUser, getUserCount } from "../controllers/users.js";
+import {
+  getUsers,
+  addUser,
+  updateUser,
+  getUser,
+  deleteUser,
+  loginUser,
+  getUserCount,
+} from "../controllers/users.js";
 
 const router = express.Router();
 
-
-router.post('/', addUser);
-router.get('/', getUsers);
-router.get('/get/count', getUserCount);
-router.get('/:userId', getUser);
-router.post('/login', loginUser);
-router.put('/', updateUser);
-router.delete('/', deleteUser);
-
+router.post("/register", addUser);
+router.get("/", getUsers);
+router.get("/get/count", getUserCount);
+router.get("/:userId", getUser);
+router.post("/login", loginUser);
+router.put("/", updateUser);
+router.delete("/", deleteUser);
 
 export default router;
